b822d3c4977f27a1957f97cda5149ceaa29af479
김경훈 <kidi052812@gmail.com>
2020-09-09 12:25:31 +0900

    [ADD] : Moira_Server_Project

---
+++ Server/Moira/Moira/Models/Job/JobModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moira.Models.Job
+{
+    public class JobModel
+    {
+        public int job_idx { get; set; }
+        public string field { get; set; }
+        public string description { get; set; }
+        public int people_num { get; set; }
+        public int is_deadline { get; set; }
+        public string writer { get; set; }
+        public string contact { get; set; }
+        public string title { get; set; }
+    }
+}
